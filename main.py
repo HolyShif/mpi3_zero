f72b902b0ee5da668c93fbcd8e9c84e827e8bbec
HolyShif <kyledshiflett@hotmail.com>
2018-03-08 05:08:56 +0000

    started wpa_supplicant.conf manipulation

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import string
 import wifi
 
 wi_name = wifi.scan()
 print wi_name
+
+#wpa_f = open('/etc/wpa_supplicant/wpa_supplicant.conf', 'r+')
+wpa_f = open('/home/pi/mpi3_zero/wpa_tst.conf', 'r+')
+data = wpa_f.read()
+
+#print data
+print (string.count(data,"SR Wireless"))
+
+wpa_f.close()
